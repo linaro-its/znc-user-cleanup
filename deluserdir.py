d1ea587094b92a8fe1cf4fe4ac93d5690bdae3c7
Philip Colmer <philip.colmer@linaro.org>
2019-03-19 15:26:49 +0000

    Add code to show user dir on demand

--- deluserdir.py
+++ deluserdir.py
@@ -60,16 +60,14 @@ class deluserdir(znc.Module):
 
         if success:
             if self.nv["trashdir"] == "":
-                znc.CZNC.Broadcast(
-                    "deluserdir is loaded. User directories will be deleted",
-                    True
+                znc.CZNC.Get().Broadcast(
+                    "deluserdir is loaded. User directories will be deleted"
                 )
             else:
-                znc.CZNC.Broadcast(
+                znc.CZNC.Get().Broadcast(
                     "deluserdir is loaded. User directories will be moved"
                     " to '%s' when user accounts are deleted" %
-                    self.nv["trashdir"],
-                    True
+                    self.nv["trashdir"]
                 )
         return success
 
@@ -78,17 +76,42 @@ class deluserdir(znc.Module):
         userdir = user.GetUserPath()
         try:
             if trashdir == "":
-                znc.CZNC.Broadcast("Deleting %s" % userdir, True)
+                znc.CZNC.Get().Broadcast("Deleting %s" % userdir)
                 shutil.rmtree(userdir)
             else:
-                znc.CZNC.Broadcast("Moving %s to trash dir" % userdir, True)
+                znc.CZNC.Get().Broadcast("Moving %s to trash dir" % userdir)
                 shutil.move(userdir, trashdir)
-            if os.path.isdir(userdir):
-                znc.CZNC.Broadcast(
-                    "Uh oh. Source directory still there.", True)
-            else:
-                znc.CZNC.Broadcast("Operation completed", True)
+            self.__output_users(True)
         except Exception as e:
-            znc.CZNC.Broadcast(
-                "deluserdir:OnDeleteUser failed with %s" % str(e), True)
+            znc.CZNC.Get().Broadcast(
+                "deluserdir:OnDeleteUser failed with %s" % str(e))
         return znc.CONTINUE
+
+    def __output_table(self, t, broadcast):
+        i = 0
+        s = znc.String()
+        while t.GetLine(i, s):
+            if broadcast:
+                znc.CZNC.Get().Broadcast(s.s)
+            else:
+                self.PutModule(s.s)
+            i += 1
+
+    def __output_users(self, broadcast):
+        try:
+            dirs = next(os.walk(znc.CZNC.Get().GetUserPath()))[1]
+            # Create a table with all of the user directories list
+            t = znc.CTable()
+            t.AddColumn("Users with directories")
+            for dir in dirs:
+                t.AddRow()
+                t.SetCell("Users with directories", dir)
+            self.__output_table(t, broadcast)
+        except Exception as e:
+            self.PutModule(
+                "__output_users failed with %s" % str(e))
+
+    def OnModCommand(self, message):
+        # For now, just provide some debugging information when we receive
+        # a private message.
+        self.__output_users(False)
